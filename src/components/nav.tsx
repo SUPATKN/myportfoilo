50c64eb98e7b71e9f667dff00611b0e75222aabd
SUPATKN <yacht1400@gmail.com>
2024-12-19 13:45:19 +0700

    add hidden navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,30 +1,59 @@
+import {
+  motion,
+  useScroll,
+  useMotionValueEvent,
+  easeInOut,
+} from "framer-motion";
+import { useEffect, useState } from "react";
 export const Nav = () => {
+  const { scrollY } = useScroll();
+  const [hidden, setHidden] = useState(false);
+  useMotionValueEvent(scrollY, "change", (lastest) => {
+    const previous = scrollY.getPrevious();
+    if (lastest > previous && lastest > 150) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+  });
+
   return (
-    <nav className="sticky top-0 min-h-20 flex flex-row justify-between items-center px-12 bg-gradient-to-b from-cyan-900/70 to-transparent z-50 animate-dropdown">
+    <motion.nav
+      variants={{ visible: { y: 0 }, hidden: { y: "-100%" } }}
+      animate={hidden ? "hidden" : "visible"}
+      transition={{ duration: 0.35, ease: easeInOut }}
+      className="sticky top-0 min-h-20 flex flex-row justify-between items-center px-12 bg-gradient-to-b from-emerald-200/90 to-transparent z-50 animate-dropdown"
+    >
       {/* ฝั่งซ้าย */}
-      <h1 className="text-4xl text-cyan-100 font-bold">Portfolio</h1>
+      <h1 className="text-4xl text-emerald-900/80 font-bold">Portfolio</h1>
 
       {/* ฝั่งขวา */}
       <div className="flex flex-row gap-x-8">
         <a
           href="#about"
-          className="text-xl text-cyan-100 font-bold hover:text-cyan-300 transform transition-transform duration-300 hover:scale-125"
+          className="text-xl text-emerald-900/80 font-bold hover:text-emerald-900 transform transition-transform duration-300 hover:scale-125"
         >
           About Me
         </a>
         <a
+          href="#tech"
+          className="text-xl text-emerald-900/80 font-bold hover:text-emerald-900 transform transition-transform duration-300 hover:scale-125"
+        >
+          Tech Skills
+        </a>
+        <a
           href="#projects"
-          className="text-xl text-cyan-100 font-bold hover:text-cyan-300 transform transition-transform duration-300 hover:scale-125"
+          className="text-xl text-emerald-900/80 font-bold hover:text-emerald-900 transform transition-transform duration-300 hover:scale-125"
         >
           Projects
         </a>
         <a
           href="#contact"
-          className="text-xl text-cyan-100 font-bold hover:text-cyan-300 transform transition-transform duration-300 hover:scale-125"
+          className="text-xl text-emerald-900/80 font-bold hover:text-emerald-900 transform transition-transform duration-300 hover:scale-125"
         >
           Contact
         </a>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
