0cbbd7d9e16f7ba115d9bd84f6430c0cc93bd00a
SUPATKN <yacht1400@gmail.com>
2024-12-21 16:23:25 +0700

    edit path

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,5 +1,5 @@
 export const Aboutme = () => {
-  const img = "/public/profilePic1.jpg";
+  const img = "dist/profilePic1.jpg";
   return (
     <div className="flex flex-col items-center w-full ">
       <img
